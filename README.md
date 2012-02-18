9ac541f7eecc204098e38d99e9de8ae39ef1f996
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2012-02-18 12:25:01 -0500

    WIP added fnames, catch more names on facebook

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Currently, to use it, you paste it into your console in Firefox, Chrome, or
 Opera
 
 ###To do
-* get "joe blow likes this" names
-* get names tagged in a post (these both have data-hovercard)
+* -get first names list- shorten this list to ~100
+* get last names list (nouns).
 * map the names to an array and replace them rather than just truncating them
 * get the names *in* the comments and replace them
 * add a demo screenshot
--- bookmarklet.js
+++ bookmarklet.js
@@ -24,10 +24,19 @@ if(typeof afonigizer == 'undefined'){
                 },
                 facebook : {
                     avatarSelector : 'img.uiProfilePhoto',
-                    nameSelector : 'a.actorName, div.actorName a, a.passiveName, span.passiveName',
+                    nameSelector : 'a.actorName, div.actorName a,' +
+						'a.passiveName, span.passiveName, a[data-hovercard]',
+                    nameFilter : function(anchor){
+                        return ( anchor.childNodes.length === 1 &&
+                                 anchor.firstChild.nodeType === Node.TEXT_NODE );
+                    },
                     hashAttribute : 'src'
                 }
             },
+			//@todo I probably don't need quite this many...
+			fNames = [
+"Abashed", "Abhorrent", "Abject", "Abnormal", "Aboriginal", "Abrupt", "Abstracted", "Accessible", "Adjoining", "Alert", "Alive", "Alluring", "Ambiguous", "Amuck", "Animated", "Aquatic", "Aspiring", "Astonishing", "Available", "Aware", "Bad", "Bashful", "Beautiful", "Berserk", "Boiling", "Boring", "Brash", "Breezy", "Bright", "Burly", "Callous", "Capricious", "Changeable", "Childlike", "Chivalrous", "Chunky", "Classy", "Cloudy", "Coherent", "Colossal", "Comfortable", "Cool", "Courageous", "Crabby", "Crazy", "Creepy", "Cuddly", "Curly", "Dapper", "Dazzling", "Debonair", "Decorous", "Deeply", "Delightful", "Deranged", "Detailed", "Devilish", "Diligent", "Dirty", "Discreet", "Dispensable", "Dizzy", "Draconian", "Dull", "Dynamic", "Eager", "Eatable", "Educated", "Efficient", "Elite", "Empty", "Encouraging", "Energetic", "Enthusiastic", "Erect", "Ethereal", "Excellent", "Exclusive", "Expensive", "Exultant", "Faded", "Fanatical", "Fantastic", "Fast", "Faulty", "Feigned", "Festive", "Finicky", "Flashy", "Flippant", "Foamy", "Forgetful", "Frail", "Friendly", "Funny", "Futuristic", "Gainful", "Gaudy", "Giant", "Glib", "Glossy", "Gorgeous", "Grandiose", "Groovy", "Grouchy", "Grumpy", "Guiltless", "Gusty", "Habitual", "Hallowed", "Handsome", "Hapless", "Harmonious", "Healthy", "Heavenly", "Hellish", "Hesitant", "Highfalutin", "Historical", "Hollow", "Horrible", "Huge", "Humdrum", "Hungry", "Hysterical", "Imaginary", "Imminent", "Imperfect", "Incandescent", "Inconclusive", "Innate", "Internal", "Irate", "Jaded", "Jazzy", "Jittery", "Jolly", "Judicious", "Jumpy", "Knowing", "Known", "Lackadaisical", "Lamentable", "Large", "Laughable", "Lazy", "Learned", "Lethal", "Lewd", "Likeable", "Little", "Long", "Lopsided", "Lovely", "Low", "Lucky", "Lush", "Lying", "Macabre", "Maddening", "Magenta", "Magnificent", "Makeshift", "Mammoth", "Meek", "Merciful", "Mighty", "Miniature", "Momentous", "Mundane", "Mushy", "Needless", "Neighborly", "New", "Nifty", "Noisy", "Nondescript", "Nostalgic", "Noxious", "Numberless", "Nutritious", "Obedient", "Obscene", "Observant", "Obtainable", "Odd", "Onerous", "Orange", "Organic", "Oval", "Overjoyed", "Overt", "Panoramic", "Parsimonious", "Pathetic", "Penitent", "Periodic", "Perpetual", "Phobic", "Piquant", "Plausible", "Plucky", "Political", "Precious", "Pretty", "Productive", "Protective", "Psychedelic", "Puffy", "Puny", "Quack", "Quarrelsome", "Quick", "Quiet", "Quizzical", "Racial", "Rainy", "Rampant", "Raspy", "Rebellious", "Recondite", "Redundant", "Relieved", "Resolute", "Rhetorical", "Rightful", "Roasted", "Romantic", "Round", "Ruddy", "Rustic", "Sable", "Sassy", "Scandalous", "Scary", "Scintillating", "Secretive", "Seemly", "Shallow", "Short", "Silly", "Skillful", "Sloppy", "Small", "Sneaky", "Snotty", "Somber", "Spectacular", "Spiffy", "Splendid", "Spurious", "Square", "Staking", "Statuesque", "Stereotyped", "Stingy", "Subdued", "Successful", "Sulky", "Swanky", "Symptomatic", "Tacit", "Talented", "Tame", "Tan", "Tangy", "Tasteful", "Tearful", "Temporary", "Tenuous", "Testy", "Thinkable", "Tight", "Tiny", "Toothsome", "Tough", "Tranquil", "Tricky", "Truculent", "Ubiquitous", "Unable", "Unadvised", "Uncovered", "Uneven", "Unsuitable", "Upbeat", "Utter", "Various", "Verdant", "Vivacious", "Volatile", "Vulgar", "Waggish", "Wanting", "Warm", "Wasteful", "Watery", "Wealthy", "Wee", "Whimsical", "Wiry", "Wistful", "Wooden", "Workable", "Yellow", "Yummy", "Zippy"
+],
             setConf = function(){
 	        if(host === 'plus.google.com') { conf = services.google; }
                 else if (host === 'www.facebook.com'){ conf = services.facebook; }
@@ -66,6 +75,17 @@ if(typeof afonigizer == 'undefined'){
                     //@todo make a funny name
                     var nameStr = fullName.innerHTML.substring(0,2);
                     fullName.innerHTML = nameStr + "...";
+					//new plan:
+					//
+					//explode the name. drop middle names. foreach part{
+						//Check if it's in nameMap:
+							//It is: break and go to replace it
+							//It isn't:
+							//	pop a new name off the unusedNames array
+						//Replace the name part in newName
+					//}
+					//replace the name on the page
+					
                 }
             },
             fixAvatars = function(avatars){
