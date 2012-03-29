705af82e129b2ba9d80914e75d9a84093c27a197
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2012-03-28 23:31:29 -0400

    fixed a selector

--- afonigizer.js
+++ afonigizer.js
@@ -35,7 +35,7 @@ var afonigizer = afonigizer || (function (window, Math, Node) {
 					', .fbChatOrderedList .item a .name' + //chat sidebar
 					', .headerTinymanName' + //top navbar name
 					', .fbxWelcomeBoxName' + //top left column user's name
-					', .fbReminders .fbRemindersStory .fbRemindersTitle', //birthday's &c.
+					', .fbReminders .fbRemindersStory .fbRemindersTitle strong', //birthday's &c.
 				textblockSelector : '.messageBody, .commentBody',
 				nameFilter : function (anchor) {
 					var success = ( anchor.childNodes.length === 1 &&
